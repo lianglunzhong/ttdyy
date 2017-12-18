b5a3ef8d4ff6f9d6da4e5e080c3a7ded6408f7ae
lianglunzhong <lianglunzhong@gmail.com>
2017-12-18 16:34:51 +0800

    admin登录页面

--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -1,9 +1,24 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>ttdyy-login</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/ttdyy.login.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-4 col-md-offset-4 login-panel">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
 
@@ -11,28 +26,28 @@
                     <form class="form-horizontal" method="POST" action="{{ route('admin.login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
+                        <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+                            <div class="col-md-12">
+                                <input type="email" name="email" class="form-control">
+                                <span class="glyphicon glyphicon-envelope login-input-icon" aria-hidden="true"></span>
+                                
                                 @if ($errors->has('email'))
                                     <span class="help-block">
+                                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
+                        <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password" required>
+                                <span class="glyphicon glyphicon-lock login-input-icon" aria-hidden="true"></span>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
+                                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -40,8 +55,8 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-info btn-block">
                                     Login
                                 </button>
                             </div>
@@ -52,4 +67,12 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+    
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
