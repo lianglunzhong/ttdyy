f7fd88fe4ba39c2df5ce7abe3cf622f7916ed44c
lianglunzhong <lianglunzhong@gmail.com>
2018-01-07 20:48:37 +0800

    多图上传

--- resources/views/admin/movies/movieCreate.blade.php
+++ resources/views/admin/movies/movieCreate.blade.php
@@ -3,6 +3,9 @@
 @section('style')
     <link rel="stylesheet" type="text/css" href="{{ asset('ttdyy-admin/bootstrap-select/css/bootstrap-select.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('ttdyy-admin/css/ttdyy.create.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ttdyy-admin/jQueryTzUpload/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ttdyy-admin/jQueryTzUpload/css/demo.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ttdyy-admin/jQueryTzUpload/webuploader-0.1.5/webuploader.css') }}">
 @endsection
 
 
@@ -85,6 +88,32 @@
                                     </select>
                                 </div>
                             </div>
+
+                            <!-- Images -->
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Images</label>
+                                <div class="col-sm-8">
+                                    <div id="uploader" class="wu-example">
+                                        <div class="queueList">
+                                            <div id="dndArea" class="placeholder">
+                                                <div id="filePicker"></div>
+                                                <p>或将照片拖到这里</p>
+                                            </div>
+                                        </div>
+                                        <div class="statusBar" style="display:none">
+                                            <div class="progress">
+                                                <span class="text">0%</span>
+                                                <span class="percentage"></span>
+                                            </div>    
+                                            <div class="info"></div>
+                                            <div class="btns">
+                                                <div id="filePicker2"></div>
+                                                <div class="uploadBtn">开始上传</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -98,6 +127,8 @@
 
 @section('script')
 <script type="text/javascript" src="{{ asset('ttdyy-admin/bootstrap-select/js/bootstrap-select.js') }}"></script>
+<script type="text/javascript" src="{{ asset('ttdyy-admin/jQueryTzUpload/webuploader-0.1.5/webuploader.min.js') }}"></script>
+
 <script type="text/javascript">
     $(function() {
         // 分类选择
@@ -111,8 +142,29 @@
             'maxOptions' : 3,
             'size' : 8
         });
+
+        window.webuploader = {
+            config:{
+                thumbWidth: 110, //缩略图宽度，可省略，默认为110
+                thumbHeight: 110, //缩略图高度，可省略，默认为110
+                wrapId: 'uploader', //必填
+            },
+            //处理客户端新文件上传时，需要调用后台处理的地址, 必填
+            uploadUrl: 'fileupload.php',
+            //处理客户端原有文件更新时的后台处理地址，必填
+            updateUrl: 'fileupdate.php',
+            //当客户端原有文件删除时的后台处理地址，必填
+            removeUrl: 'filedel.php',
+            //初始化客户端上传文件，从后台获取文件的地址, 可选，当此参数为空时，默认已上传的文件为空
+            initUrl: 'fileinit.php',
+        }
+
+        $('.uploadBtn').click(function() {
+            console.log(1111);
+        })
     });
 
 
 </script>
+<script type="text/javascript" src="{{ asset('ttdyy-admin/jQueryTzUpload/webuploader-0.1.5/extend-webuploader.js') }}"></script>
 @endsection
